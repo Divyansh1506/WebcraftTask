da7bf109132ed4e07e0bbc8be6d6297663fe90da
Divyansh Sarathe <96368459+Divyansh1506@users.noreply.github.com>
2024-01-23 14:35:05 +0530

    Update firebase-config.js

--- src/Component/firebase-config.js
+++ src/Component/firebase-config.js
@@ -2,11 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 
-// const firebaseConfig = {
-//   apiKey: "Your Api Key",           // Replace 
-//   authDomain: "Your Auth Domain",   // Replace
-// };
-
 const firebaseConfig = {
   apiKey: "AIzaSyCpxto_ZJ9W_jSCSLUnMbfXkI_7Xm6uCQ0",
   authDomain: "loginbymail-942fd.firebaseapp.com",
